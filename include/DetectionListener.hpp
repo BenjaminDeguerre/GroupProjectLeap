ea35725358576136626845ea3f5e5608f8a99188
BenjaminDeguerre <benjamin.deguerre@insa-rouen.fr>
2021-02-12 23:36:41 +0100

    cleaning the file

--- include/DetectionListener.hpp
+++ include/DetectionListener.hpp
@@ -3,34 +3,98 @@
 
 #define _CRT_SECURE_NO_DEPRECATE
 
-#include <iostream>
 #include <cstring>
+#include <iostream>
 
-#include "Leap.h"
-#include "StaticGestures.hpp"
 #include "GestValidator.hpp"
+#include "Leap.h"
 #include "ModeHandler.hpp"
+#include "StaticGestures.hpp"
 
+/**
+ * \class DetectionListener
+ * \brief Handles the differents event that can occur during a session.
+ **/
 class DetectionListener : public Leap::Listener {
-private:
-	ModeHandler handler;
-	GestValidator validator;
+ private:
+  ModeHandler handler;
+  GestValidator validator;
 
-	int mode = 0;
-	bool selectingMode;
+  int mode = 0;
+  bool selectingMode;
 
-public:
-	DetectionListener(): selectingMode(true) {}
-	virtual void onInit(const Leap::Controller&);
-	virtual void onConnect(const Leap::Controller&);
-	virtual void onDisconnect(const Leap::Controller&);
-	virtual void onExit(const Leap::Controller&);
-	virtual void onFrame(const Leap::Controller&);
-	virtual void onFocusGained(const Leap::Controller&);
-	virtual void onFocusLost(const Leap::Controller&);
-	virtual void onDeviceChange(const Leap::Controller&);
-	virtual void onServiceConnect(const Leap::Controller&);
-	virtual void onServiceDisconnect(const Leap::Controller&);
+ public:
+  DetectionListener() : selectingMode(true) {}
+
+  /**
+   * \fn void DetectionListener::onInit(const Leap::Controller& controller)
+   * \brief Called on init, does nothing.
+   **/
+  virtual void onInit(const Leap::Controller&);
+
+  /**
+   * \fn void DetectionListener::onConnect(const Leap::Controller& controller)
+   * \brief Enables the four dynamic gestures used to control the robot.
+   **/
+  virtual void onConnect(const Leap::Controller&);
+
+  /**
+   * \fn void DetectionListener::onDisconnect(const Leap::Controller&)
+   * \brief Called when the Listener disconnects, does nothing.
+   **/
+  virtual void onDisconnect(const Leap::Controller&);
+
+  /**
+   * \fn void DetectionListener::onExit(const Leap::Controller& controller)
+   * \brief Called on exit, does nothing.
+   **/
+  virtual void onExit(const Leap::Controller&);
+
+  /**
+   * \fn void DetectionListener::onFrame(const Leap::Controller& controller)
+   * \brief Wait for a new frame and either switch between modes or send command
+   *to the robot depending on the mode. If no mode is selected we wait for
+   *either ONE_FINGER_RIGHT_HAND or TWO_FINGERS_RIGHT_HAND to select mode one or
+   *two. If a mode is selected but not validated, we wait for GRAB_ONE_HAND or
+   *FIVE_FINGERS_RIGHT_HAND to validate or go back to selection. Else, we
+   *call the correct function.
+   **/
+  virtual void onFrame(const Leap::Controller&);
+
+  /**
+   * \fn void DetectionListener::onFocusGained(const Leap::Controller&
+   *controller)
+   * \brief Called when the focus is gained, does nothing.
+   **/
+  virtual void onFocusGained(const Leap::Controller&);
+
+  /**
+   * \fn void DetectionListener::onFocusLost(const Leap::Controller& controller)
+   * \brief Called when the focus is lost, does nothing.
+   **/
+  virtual void onFocusLost(const Leap::Controller&);
+
+  /**
+   * \fn void DetectionListener::onDeviceChange(const Leap::Controller&
+   *controller)
+   * \brief Just print stuff if the device is changed/lost and found
+   *again.
+   **/
+  virtual void onDeviceChange(const Leap::Controller&);
+
+  /**
+   * \fn void DetectionListener::onServiceConnect(const Leap::Controller&
+   *controller)
+   * \brief Called when the service connects, does nothing.
+   **/
+  virtual void onServiceConnect(const Leap::Controller&);
+
+  /**
+   * \fn void DetectionListener::onServiceDisconnect(const Leap::Controller&
+   *controller)
+   * \brief Called when the service disconnects, does nothing.
+   **/
+  virtual void onServiceDisconnect(const Leap::Controller&);
 };
 
 #endif
--- src/DetectionListener.cpp
+++ src/DetectionListener.cpp
@@ -1,102 +1,104 @@
 #include "DetectionListener.hpp"
 
 void DetectionListener::onInit(const Leap::Controller& controller) {
-	std::cout << "Initialized" << std::endl;
+  std::cout << "Initialized" << std::endl;
 }
 
 void DetectionListener::onConnect(const Leap::Controller& controller) {
-	std::cout << "Connected" << std::endl;
-	controller.enableGesture(Leap::Gesture::TYPE_CIRCLE);
-	controller.enableGesture(Leap::Gesture::TYPE_KEY_TAP);
-	controller.enableGesture(Leap:: Gesture::TYPE_SCREEN_TAP);
-	controller.enableGesture(Leap::Gesture::TYPE_SWIPE);
+  std::cout << "Connected" << std::endl;
+  controller.enableGesture(Leap::Gesture::TYPE_CIRCLE);
+  controller.enableGesture(Leap::Gesture::TYPE_KEY_TAP);
+  controller.enableGesture(Leap::Gesture::TYPE_SCREEN_TAP);
+  controller.enableGesture(Leap::Gesture::TYPE_SWIPE);
 }
 
 void DetectionListener::onDisconnect(const Leap::Controller& controller) {
-	// Note: not dispatched when running in a debugger.
-	std::cout << "Disconnected" << std::endl;
+  // Note: not dispatched when running in a debugger.
+  std::cout << "Disconnected" << std::endl;
 }
 
 void DetectionListener::onExit(const Leap::Controller& controller) {
-	std::cout << "Exited" << std::endl;
+  std::cout << "Exited" << std::endl;
 }
 
 void DetectionListener::onFrame(const Leap::Controller& controller) {
+  // Get the most recent frame and report some basic information
+  const Leap::Frame frame = controller.frame();
+  StaticGestures sGestures(frame.hands());
+  StaticGesture sGesture = sGestures.getGesture();
+  validator.update(sGesture);
+  sGesture = validator.getCurrentValidGesture();
 
-	// Get the most recent frame and report some basic information
-	const Leap::Frame frame = controller.frame();
-	StaticGestures sGestures(frame.hands());
-	StaticGesture sGesture = sGestures.getGesture();
-	validator.update(sGesture);
-	sGesture = validator.getCurrentValidGesture();
-
-	if (mode == 0) {
-		switch (sGesture) {
-			case ONE_FINGER_RIGHT_HAND:
-				//mode 1 - select text and print it on the screen
-				std::cout << "selecting mode 1" << '\n';
-				mode = 1;
-				validator.setGesture(ERROR_SG); //setting to new gesture
-			break;
-			case TWO_FINGERS_RIGHT_HAND:
-				//mode 2 - writing with gestures
-				std::cout << "selecting mode 2" << '\n';
-				mode = 2;
-				validator.setGesture(ERROR_SG); //setting to new gesture
-			break;
-			default:
-				//do nothing and wait for detection.	
-			break;
-		}
-	} else if (mode != 0 && selectingMode) {
-		if (sGesture == GRAB_ONE_HAND) {
-			std::cout << "Activating selected mode" << '\n';
-			selectingMode = false;
-			validator.setGesture(ERROR_SG);
-		} else if (sGesture == FIVE_FINGERS_RIGHT_HAND) {
-			std::cout << "New selection" << '\n';
-			mode = 0;
-			validator.setGesture(ERROR_SG);
-		}
-	} else {
-		switch (mode) {
-			case 1:
-			selectingMode = !handler.mode1(sGesture);
-			break;
-			case 2:
-			selectingMode = !handler.mode2(sGesture, frame.gestures(), frame.fingers());
-			break;
-			default:
-			break;
-		}
-		if (selectingMode) {
-			mode = 0;
-		}
-	}
+  if (mode == 0) {
+    switch (sGesture) {
+      case ONE_FINGER_RIGHT_HAND:
+        // mode 1 - select text and print it on the screen
+        std::cout << "selecting mode 1" << '\n';
+        mode = 1;
+        validator.setGesture(ERROR_SG);  // setting to new gesture
+        break;
+      case TWO_FINGERS_RIGHT_HAND:
+        // mode 2 - writing with gestures
+        std::cout << "selecting mode 2" << '\n';
+        mode = 2;
+        validator.setGesture(ERROR_SG);  // setting to new gesture
+        break;
+      default:
+        // do nothing and wait for detection.
+        break;
+    }
+  } else if (mode != 0 && selectingMode) {
+    if (sGesture == GRAB_ONE_HAND) {
+      std::cout << "Activating selected mode" << '\n';
+      selectingMode = false;
+      validator.setGesture(ERROR_SG);
+    } else if (sGesture == FIVE_FINGERS_RIGHT_HAND) {
+      std::cout << "New selection" << '\n';
+      mode = 0;
+      validator.setGesture(ERROR_SG);
+    }
+  } else {
+    switch (mode) {
+      case 1:
+        selectingMode = !handler.mode1(sGesture);
+        break;
+      case 2:
+        selectingMode =
+            !handler.mode2(sGesture, frame.gestures(), frame.fingers());
+        break;
+      default:
+        break;
+    }
+    if (selectingMode) {
+      mode = 0;
+    }
+  }
 }
 
 void DetectionListener::onFocusGained(const Leap::Controller& controller) {
-	std::cout << "Focus Gained" << std::endl;
+  std::cout << "Focus Gained" << std::endl;
 }
 
 void DetectionListener::onFocusLost(const Leap::Controller& controller) {
-	std::cout << "Focus Lost" << std::endl;
+  std::cout << "Focus Lost" << std::endl;
 }
 
 void DetectionListener::onDeviceChange(const Leap::Controller& controller) {
-	std::cout << "Device Changed" << std::endl;
-	const Leap::DeviceList devices = controller.devices();
+  std::cout << "Device Changed" << std::endl;
+  const Leap::DeviceList devices = controller.devices();
 
-	for (int i = 0; i < devices.count(); ++i) {
-		std::cout << "id: " << devices[i].toString() << std::endl;
-		std::cout << "  isStreaming: " << (devices[i].isStreaming() ? "true" : "false") << std::endl;
-	}
+  for (int i = 0; i < devices.count(); ++i) {
+    std::cout << "id: " << devices[i].toString() << std::endl;
+    std::cout << "  isStreaming: "
+              << (devices[i].isStreaming() ? "true" : "false") << std::endl;
+  }
 }
 
 void DetectionListener::onServiceConnect(const Leap::Controller& controller) {
-	std::cout << "Service Connected" << std::endl;
+  std::cout << "Service Connected" << std::endl;
 }
 
-void DetectionListener::onServiceDisconnect(const Leap::Controller& controller) {
-	std::cout << "Service Disconnected" << std::endl;
+void DetectionListener::onServiceDisconnect(
+    const Leap::Controller& controller) {
+  std::cout << "Service Disconnected" << std::endl;
 }
